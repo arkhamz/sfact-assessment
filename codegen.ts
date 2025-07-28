836b5a479957dcb4252cd62a7919b1306d79817d
arkhamz <68877605+arkhamz@users.noreply.github.com>
2025-07-28 14:21:02 +0200

    codegen-graphql-types

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,9 @@
+import "dotenv/config";
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: import.meta.env.VITE_SCHEMA_URL,
+  // schema: import.meta.env.VITE_SCHEMA_URL,
+  schema: process.env.VITE_SCHEMA_URL,
   // this assumes that all your source files are in a top-level `src/` directory - you might need to adjust this to your file structure
   documents: ["src/**/*.{ts,tsx}"],
   generates: {
