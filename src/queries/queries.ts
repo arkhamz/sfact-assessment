f4e2ea180c4c217d92ae29d5ad360b33dc6a3410
arkhamz <68877605+arkhamz@users.noreply.github.com>
2025-07-28 14:42:49 +0200

    cleanup-some-bootstrap-files-and-add-first-query

---
+++ src/queries/queries.ts
@@ -0,0 +1,19 @@
+import { gql } from "../__generated__/gql";
+
+export const GET_LAUNCHES = gql(`
+
+query GetLaunches{
+  launchesPast(limit:10){
+    mission_name
+    launch_date_utc
+    rocket{
+        rocket_name
+        rocket{
+            mass {
+                kg
+            }
+        }
+    }
+  }
+}
+    `);
